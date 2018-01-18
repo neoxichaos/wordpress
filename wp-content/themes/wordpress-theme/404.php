159682cdb47c01ee4d8fd6beb6a5a86f701540de
Fsimmet3 <floriane.simmet@gmail.com>
2018-01-18 11:41:06 +0100

    erreur 404

--- wp-content/themes/wordpress-theme/404.php
+++ wp-content/themes/wordpress-theme/404.php
@@ -1,1 +1,9 @@
-<h1>erreur 404</h1>
+<?php wp_head(); ?>
+<div class="container-fluid">
+  <div class="container">
+    <div class="back">
+      <a href="<?php bloginfo('url')?>" class="btn btn-secondary">Accueil</a>
+    </div>
+    <h1 class="text-center error">Erreur 404</h1>
+  </div>
+</div>
